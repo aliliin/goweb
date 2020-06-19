d4387fb70ca4aaf47d9b858a9cb9e583c615affc
高永立 <phperali@gmail.com>
2020-06-19 17:13:59 +0800

    code refactoring

---
+++ common/database.go
@@ -0,0 +1,41 @@
+package common
+
+import (
+	"fmt"
+	"github.com/jinzhu/gorm"
+	"goweb/learngin/model"
+)
+
+var DB *gorm.DB
+// 开启链接池
+func InitDB() *gorm.DB {
+
+	driverName := "mysql"
+	host := "127.0.0.1"
+	port := "3306"
+	database := "gin"
+	username := "root"
+	password := ""
+	charset := "utf8"
+
+	args := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s&parseTime=true",
+		username,
+		password,
+		host,
+		port,
+		database,
+		charset)
+
+	db, err := gorm.Open(driverName, args)
+	if err != nil {
+		panic("failed to connect database, err: " + err.Error())
+	}
+	// 自动创建表
+	db.AutoMigrate(&model.User{})
+	DB= db
+	return db
+}
+
+func GetDB() *gorm.DB{
+	return DB
+}
